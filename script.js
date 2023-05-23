6d29af4d13d8da878965f0871238ba6db067db31
Adse3200 <134303027+Adse3200@users.noreply.github.com>
2023-05-23 17:05:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function blabla(){
-
-
-}
+var button = document.getElementById("button1");
+button.addEventListener("click", function(e) {
+    // e is the event object
+    // e.target is the button element
+    // do stuff with them
+})
